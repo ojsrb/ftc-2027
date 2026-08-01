420a7532d4b7a78bd82be27ddb60418b92388f12
Owen <owenschmidt2010@gmail.com>
2026-07-31 21:27:02 -0500

    suppress warnings

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/MainTeleOp.java
@@ -3,7 +3,6 @@ package org.firstinspires.ftc.teamcode.teleop;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.util.ElapsedTime;
 import com.seattlesolvers.solverslib.gamepad.GamepadEx;
 import com.seattlesolvers.solverslib.kinematics.wpilibkinematics.ChassisSpeeds;
 
@@ -13,8 +12,6 @@ import org.firstinspires.ftc.teamcode.Superstructure;
 @TeleOp(name = "MainTeleOp", group = "TeleOp")
 @Disabled
 public class MainTeleOp extends OpMode {
-    private final ElapsedTime runtime = new ElapsedTime();
-
     private GamepadEx driverGamepad;
 
     private Superstructure superstructure;
